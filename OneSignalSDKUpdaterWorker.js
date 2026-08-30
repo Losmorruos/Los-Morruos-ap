78188cf0055788596329140673d23c04aa20bdac
Losmorruos <clubdeportivolosmorruos@gmail.com>
2026-08-30 01:50:10 +0100

    Add files via upload

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,2 @@
+// Updater Worker de OneSignal.
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,3 @@
+// Service Worker dedicado para OneSignal (requerido por el SDK en algunos navegadores).
+// Reenvía al worker oficial de OneSignal.
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
