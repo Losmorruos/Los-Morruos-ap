fbbd11c6dda245eb75bc12d08082af3e8b402aec
Losmorruos <clubdeportivolosmorruos@gmail.com>
2026-09-01 01:26:21 +0100

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,3 +1,1 @@
-// OneSignal service worker for GitHub Pages (subpath /Los-Morruos-ap/)
-// https://documentation.onesignal.com/docs/web-sdk-setup
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
